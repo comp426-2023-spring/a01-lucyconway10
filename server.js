5f137ab538007ebd4d1f028af0c91de3443f2e64
lucyconway <lucymac@live.unc.edu>
2023-01-24 17:28:13 -0500

    port issue

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const http = require('http');
 // Require fs module
 const fs = require('fs');
 
+const minimist = require('minimist');
 // Require minimist module (make sure you install this one via npm).
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const args = require('minimist')(process.argv.slice(2));
-const port = args.port || 3000
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000;
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 

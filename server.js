61eacd6b9ca3f609dc5c0a2aff3466f8d9759350
lucyconway <lucymac@live.unc.edu>
2023-01-24 17:30:10 -0500

    maybe the word port

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const server = http.createServer((req, res) => {
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
 server.listen(port, () => {
-	console.log(`Server listening on ${port}`);
+	console.log(`Server listening on port ${port}`);
   })
 });
 
